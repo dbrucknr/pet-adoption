4f25fc8197053ec445988722c7a4d5a795c78def
Derek Bruckner <derek.bruckner@detroitlabs.com>
2021-10-14 15:54:03 -0400

    add basic functionality to websocket - enables tracking of users, and ability to send / receive messages.

--- web-socket/index.js
+++ web-socket/index.js
@@ -4,6 +4,34 @@ const io = require("socket.io")(8100, {
     },
 });
 
+const USERS = [];
+
+const setUserOnline = (userId, socketId) => 
+    !USERS.some((user) => user.userId === userId) && USERS.push({ userId, socketId });
+
+const setUserOffline = socketId => 
+    USERS = USERS.filter((user) => user.socketId !== socketId);
+
+const findUser = userId => 
+    USERS.find((user) => user.userId === userId)
+
 io.on("connection", (socket) => {
     console.log("Connection Detected");
+
+    socket.on("setUserOnline", (userId) => {
+        setUserOnline(userId, socket.id);
+        io.emit("getOnlineUsers", USERS);
+    });
+
+    socket.on("sendMessage", ({ senderId, receiverId, message }) => {
+        const receiver = findUser(receiverId);
+        io.to(receiver.socketId).emit("message", {
+            senderId, message
+        });
+    });
+
+    socket.on("disconnect", () => {
+        setUserOffline(socket.id);
+        io.emit("getOnlineUsers", USERS);
+    })
 })
